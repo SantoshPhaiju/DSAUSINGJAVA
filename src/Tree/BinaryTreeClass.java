b6262e79d134dc0860aa054dfad7ce2bf9401a4c
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-14 20:58:20 +0545

    inorder traversal done.

--- src/Tree/BinaryTreeClass.java
+++ src/Tree/BinaryTreeClass.java
@@ -39,12 +39,28 @@ public class BinaryTreeClass {
         preOrder(root.right);
     }
 
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+
+
+    }
+
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        System.out.println("PreOrder Traversal: ");
         preOrder(root);
+
+        System.out.println("InOrder Traversal: ");
+        inOrder(root);
     }
 }
  
