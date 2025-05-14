8803aa6edfa55e8b759da3b4a9449fc64895ec88
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-14 20:48:29 +0545

    binary tree build code done.

--- src/Tree/BinaryTreeClass.java
+++ src/Tree/BinaryTreeClass.java
@@ -14,8 +14,18 @@ public class BinaryTreeClass {
     }
 
     static class BinaryTree {
+        static int idx = -1;
         public static Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1){
+                return null;
+            }
 
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
         }
     }
 
@@ -23,6 +33,7 @@ public class BinaryTreeClass {
         int[] nodes = {1, 3, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
     }
 }
  
