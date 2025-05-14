1ea9ec588ef1dd36f9f3048eb37bb3adb734d63e
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-14 20:54:47 +0545

    preorder traversal done.

--- src/Tree/BinaryTreeClass.java
+++ src/Tree/BinaryTreeClass.java
@@ -15,9 +15,10 @@ public class BinaryTreeClass {
 
     static class BinaryTree {
         static int idx = -1;
+
         public static Node buildTree(int[] nodes) {
             idx++;
-            if (nodes[idx] == -1){
+            if (nodes[idx] == -1) {
                 return null;
             }
 
@@ -29,11 +30,21 @@ public class BinaryTreeClass {
         }
     }
 
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
     public static void main(String[] args) {
-        int[] nodes = {1, 3, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        preOrder(root);
     }
 }
  
