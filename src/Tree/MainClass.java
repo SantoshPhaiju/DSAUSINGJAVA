aa4e80ac374f90dfcf1b00abda192b5bc5033a35
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-17 13:11:54 +0545

    binary treeeeee

---
+++ src/Tree/MainClass.java
@@ -0,0 +1,21 @@
+package Tree;
+
+public class MainClass {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
