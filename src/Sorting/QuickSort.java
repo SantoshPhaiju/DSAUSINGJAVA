9239cd7eef6efe96c0367086bfb553950f0e7e39
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-07 17:13:19 +0545

    Correct code implemented.

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -13,16 +13,21 @@ public class QuickSort {
 
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        for (int i = low; i < high; i++) {
-            if (arr[i] < pivot) {
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
                 int temp = arr[i];
-                arr[i] = arr[low];
-                arr[low] = temp;
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        arr[high] = arr[low];
-        arr[low] = pivot;
-        return low;
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
     }
 
 
