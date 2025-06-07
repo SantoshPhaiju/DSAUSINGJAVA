2c2836b71e3761afbc3b0f23770d5420a12f713a
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-07 17:07:01 +0545

    sorting algorithm implemented but not right.

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,7 +1,33 @@
 package Sorting;
 
 public class QuickSort {
-    public static void main(String[] args) {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
 
+    public static int partition(int[] arr, int low, int high) {
+
+        int pivot = arr[high];
+        for (int i = low; i < high; i++) {
+            if (arr[i] < pivot) {
+                int temp = arr[i];
+                arr[i] = arr[low];
+                arr[low] = temp;
+            }
+        }
+        arr[high] = arr[low];
+        arr[low] = pivot;
+        return low;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 5, 2, 8};
+
+        quickSort(arr, 0, arr.length - 1);
     }
 }
