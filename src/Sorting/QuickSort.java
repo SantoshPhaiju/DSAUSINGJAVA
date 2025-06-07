1e7fcafc93c76402e3ce6d6cf1faf79241d0fd42
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-07 17:08:20 +0545

    printing the sorted array

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,5 +1,7 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class QuickSort {
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
@@ -10,7 +12,6 @@ public class QuickSort {
     }
 
     public static int partition(int[] arr, int low, int high) {
-
         int pivot = arr[high];
         for (int i = low; i < high; i++) {
             if (arr[i] < pivot) {
@@ -29,5 +30,6 @@ public class QuickSort {
         int[] arr = {6, 3, 9, 5, 2, 8};
 
         quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }
